7bb2ddf37be41dc425a06448aa8b89fd780d2567
blakelower <blakelower1999@gmail.com>
2019-10-29 22:21:40 +0700

    fous

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 // Your code goes here
+//mouseover
 let nav = document.querySelector('nav');
 nav.addEventListener("mouseover", function(event){
     event.target.style.color = "pink";
+});
+//focus
+const password = document.querySelector('input[type="password"]');
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'grey';    
+});
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+});
+//dbclick
+const card = document.querySelector('aside');
+
+card.addEventListener('dblclick', function (e) {
+  card.classList.toggle('large');
 });
