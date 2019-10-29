5d9dce2713584dd2e74effc0addfc1f17187a736
blakelower <blakelower1999@gmail.com>
2019-10-29 22:58:49 +0700

    i made a cute box using wheel

--- js/index.js
+++ js/index.js
@@ -71,3 +71,20 @@ document.addEventListener("drop", function(event) {
     event.target.appendChild( dragged );
   }
 }, false);
+
+//wheel
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('div');
+  el.onwheel = zoom;
